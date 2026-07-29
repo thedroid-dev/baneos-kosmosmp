4dc7f8170a79f9be32a480470e5204e07caa7f00
Almighty <eduardobodo751@gmail.com>
2026-07-29 09:32:02 -0600

    Update bans.js

--- api/bans.js
+++ api/bans.js
@@ -1,3 +1,4 @@
+
 import mysql from 'mysql2/promise';
 
 export default async function handler(req, res) {
@@ -20,6 +21,6 @@ export default async function handler(req, res) {
         await connection.end();
         return res.status(200).json(rows);
     } catch (error) {
-        return res.status(500).json({ error: error.message });
+        return res.status(200).json({ error: true, message: error.message });
     }
 }
